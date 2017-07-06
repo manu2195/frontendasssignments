fd8e8b4138f6ac9ce7bb95e66f1fc9ca72e3a0c4
Manishi <manishigoel25@gmail.com>
2017-07-06 20:12:41 +0530

    cat clicker version 1

--- catclicker.js
+++ catclicker.js
@@ -1,1 +1,7 @@
-alert("hello");
+var elem = document.getElementById('my-elem');
+elem.addEventListener('click',function(){
+    var counterNew= document.getElementById('counter').innerText;
+    var counterLatest=Number(counterNew)+1;
+    document.getElementById('counter').innerText=counterLatest;
+      
+},false);
