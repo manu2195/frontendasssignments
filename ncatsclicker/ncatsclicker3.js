ec5bb1ecbcdff3dd8c2bbf0457159e6f7db23692
Manishi <manishigoel25@gmail.com>
2017-07-18 15:23:05 +0530

    n cats clicker version 4

--- ncatsclicker/ncatsclicker3.js
+++ ncatsclicker/ncatsclicker3.js
@@ -1,43 +1,60 @@
 var model = {
-        catObject:[ {address: "https://i.ytimg.com/vi/3dzUgmpXPX0/hqdefault.jpg", count:0, name: "catty"},
+        data:[ {address: "https://i.ytimg.com/vi/3dzUgmpXPX0/hqdefault.jpg", count:0, name: "catty"},
                 {address: "https://s-media-cache-ak0.pinimg.com/736x/7a/ee/6f/7aee6f1493f9a8e9da7372c410783e39--cute-cats-adorable-animals.jpg", count:0,name: "waffy"},
                 {address: "http://images6.fanpop.com/image/photos/33700000/Cute-kittens-33701174-500-468.jpg", count: 0, name: "tucky"},
                 {address: "https://s-media-cache-ak0.pinimg.com/736x/8c/6e/da/8c6eda27fdc4f81c4390bc9e1bafba81--super-cute-kittens-cute-kittens-and-cats.jpg",count:0, name: "mishi"},
-                {address: "http://images4.fanpop.com/image/photos/16100000/Cute-Kittens-kittens-16123995-500-313.jpg",count:0, name: "micky"}]
-        };
+                {address: "http://images4.fanpop.com/image/photos/16100000/Cute-Kittens-kittens-16123995-500-313.jpg",count:0, name: "micky"}],
+        
+
+    getData: {
+        getCat: function (index) {
+            return model.data[index];
+        },
+
+        addCat:  function (cat) {
+            model.data.push(cat);
+        },
+
+        getCats: function () {
+            return model.data;
+        }
+    }
+};
+
 
     var octopus = {
         currentCat:null,
+        cats: undefined,
     
         init: function() {
-
-            currentCat= model.catObject[0];
-            //model.init();
-            view.init();
+            octopus.currentCat= model.getData.getCat(0); 
+            octopus.cats = model.getData.getCats();
+            view.init(octopus.cats,octopus.currentCat);
         },
         getCurrentCat: function(){
-             return currentCat;
+             return octopus.currentCat;
         },
 
         setCurrentCat: function(cat){
-                currentCat=cat;
-        },
-
-        getAllCats: function() {
-            return model.catObject;
+                octopus.currentCat=cat;
         },
 
         onClickImageView: function(){
-            currentCat.count++;
-            view.render.catView();
+            octopus.currentCat.count++;
+            view.render.catView(octopus.currentCat);
         },
+
+        onClickListRow: function function_name(index) {
+            octopus.currentCat = index;
+            view.render.catView(octopus.cats[index]);
+        }
 
     };
 var view = {
     dom_elem: {catlist_elem:null, heading_elem:null, count_elem:null, display_region_elem:null, Dom_img:null},
-    init: function () {
+    init: function (cats, currentCat) {
         view.dom_elem.catlist_elem=document.getElementById('catlist-elem');
-        view.render.listView();
+        view.render.listView(cats);
         view.dom_elem.heading_elem=document.getElementById('heading-elem');
         view.dom_elem.count_elem=document.getElementById('count-elem');
         view.dom_elem.display_region_elem=document.getElementById('display-region-elem');
@@ -46,19 +63,17 @@ var view = {
             view.dom_elem.Dom_img.addEventListener('click',function(){
                 octopus.onClickImageView();
             });
-        view.render.catView();
+        view.render.catView(currentCat);
     },
 
     render: {
-        catView: function () {
-               view.dom_elem.heading_elem.textContent=octopus.getCurrentCat().name;
-               
-               view.dom_elem.Dom_img.src=octopus.getCurrentCat().address;
-               view.dom_elem.count_elem.textContent=octopus.getCurrentCat().count;
+        catView: function (cat) {
+               view.dom_elem.heading_elem.textContent=cat.name;
+               view.dom_elem.Dom_img.src=cat.address;
+               view.dom_elem.count_elem.textContent=cat.count;
         },
 
-        listView: function () {
-            var cats=octopus.getAllCats();
+        listView: function (cats) {
             for(var i = 0; i<cats.length; i++)
             {
                 var elem = document.createElement('li');
@@ -66,10 +81,10 @@ var view = {
                 elem.addEventListener('click',(function (numCopy){
                     return function (){
                         octopus.setCurrentCat(cats[numCopy]);
-                        view.render.catView();
+                         view.render.catView(cats[numCopy]);
                     }
                 })(i));
-                view.dom_elem.catlist_elem.appendChild(elem); 
+                view.dom_elem.catlist_elem.appendChild(elem); //todo not render optimized
             }
                       
         }
